f877ace4a499b52207f503f8230fe1b0be32f51e
Ernest Guevarra <ernest@guevarra.io>
2020-08-17 21:48:06 +0100

    update package documentation

---
+++ R/odktools.R
@@ -0,0 +1,16 @@
+################################################################################
+#
+#'
+#' Open Data Kit API client in R
+#'
+#' Open Data Kit (ODK) is an open-source suite of tools that helps in authoring,
+#' collecting, and managing mobile data collection solutions. This package
+#' provides an ODK API client in R.
+#'
+#' @docType package
+#' @name odktools
+#' @keywords internal
+#'
+#
+################################################################################
+"_PACKAGE"
